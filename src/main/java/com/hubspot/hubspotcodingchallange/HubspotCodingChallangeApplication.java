3a9c0fe646b04bd427bfd5e72d367c8c5e512aa7
gajaharan <gajaharan@gmail.com>
2023-09-19 08:54:13 +0100

    initial commit

---
+++ src/main/java/com/hubspot/hubspotcodingchallange/HubspotCodingChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.hubspot.hubspotcodingchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubspotCodingChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubspotCodingChallangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hubspot/hubspotcodingchallange/HubspotCodingChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubspot.hubspotcodingchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubspotCodingChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
